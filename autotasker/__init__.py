d4139be69678cc293cbd91cc3423abd69a436ada
mramosg7 <marioramos.cobisa@gmail.com>
2024-10-14 17:23:53 +0200

    feat(docker): add new function to create Docker containers
    This commit introduces a new function responsible for creating Docker containers. It allows users to specify configurations and manage the lifecycle of Docker containers efficiently.

--- autotasker/__init__.py
+++ autotasker/__init__.py
@@ -1,5 +1,6 @@
 import click
-from autotasker.scripts.prueba import prueba
+from autotasker.managers.docker_manager import DockerManager
+
 
 
 @click.group()
@@ -9,22 +10,54 @@ def cli():
 
 
 @cli.command()
-@click.argument('filename')
-def docker(filename):
+@click.argument('path')
+@click.option('--only-image', is_flag=True, default=False, help='Creates only the image, without starting the container.')
+def docker(path, only_image):
     """Crea un contenedor de docker"""
-    click.echo(f'El archivo que quieres convertir en imagen es: {filename}')
+
+    click.echo(click.style(" Select the programming language:", bold=True, fg='cyan'))
+    languages = ['Django', 'Vite', 'React']
+
+    for i, lang in enumerate(languages, start=1):
+        click.echo(click.style(f'  {i}. {lang}', fg='yellow'))
+    lang_choice = click.prompt(click.style("", fg='green'), type=int)
+    if lang_choice < 1 or lang_choice > len(languages):
+        click.echo(click.style(" Error: Invalid choice. Please try again.", fg='red'))
+        return
+
+    selected_lang = languages[lang_choice - 1]
+    image_name = click.prompt(click.style(' Enter the name of the Docker image:', fg='green'))
+    container_name = click.prompt(click.style(' Enter the name of the Docker container:', fg='green'))
+    port = click.prompt(click.style(' Enter the port number:', fg='green'), type=int)
+
+    dockermanager = DockerManager(path, selected_lang, image_name, port, container_name)
+
+    # Create the Dockerfile
+    dockermanager.create_dockerfile()
+
+    # Create the Image
+    dockermanager.create_image()
+
+    # Creaate Container
+    if not only_image:
+        dockermanager.create_container()
 
 
-'''Hay que añadir contexto en la database'''
+
+# Hay que añadir contexto en la database
 
 
 @cli.group()
 def database():
     """Comandos para gestionar bases de datos."""
+
+
 @database.command()
 def importar_info():
     """Importar datos a una base de datos"""
     click.echo("datos importados")
+
+
 @database.command()
 def copias_seguridad():
     """Crea copias de seguridad"""
